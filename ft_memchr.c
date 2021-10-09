8cca6e3bed85aee1bcf98cfe3411a23d2d4d0a74
demicuz <pure.demicube@gmail.com>
2021-10-09 03:04:56 +0300

    added ft_memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*p;
+
+	p = s;
+	c &= 0b11111111;
+	if (n == 0)
+		return (NULL);
+	while (*p != c && n - 1)
+	{
+		p++;
+		n--;
+	}
+	if (*p != c)
+		return (NULL);
+	else
+		return ((void *) p);
+}
