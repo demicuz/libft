a2f0f1e7f94bf5c969f3f493bd7a492660cb2ea5
demicuz <pure.demicube@gmail.com>
2021-10-10 01:53:17 +0300

    .

---
+++ ft_strtrim.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psharen <psharen@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2000/01/01 20:20:20 by psharen           #+#    #+#             */
+/*   Updated: 2000/01/01 20:20:20 by psharen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static int ft_in(char const *set, char c)
+{
+	while (*set && *set != c)
+		set++;
+	return (*set == c);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t		len;
+	char const	*start;
+	char const	*end;
+
+	while (ft_in(set, *s1))
+		s1++;
+	start = s1;
+
+	while (*s1)
+		s1++;
+	while (s1[-1] && s1 > start)
+	{
+		s1--;
+	}
+	// TODO
+}
