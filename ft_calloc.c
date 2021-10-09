9c89d3f79706bb74ceee6d0674433f4442181794
demicuz <pure.demicube@gmail.com>
2021-10-09 03:32:50 +0300

    .

---
+++ ft_calloc.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*p;
+
+	p = malloc(nmemb * size);
+	if (!p)
+		return (NULL);
+	while (size)
+	{
+
+	}
+	return (NULL);
+}
--- main.c
+++ main.c
@@ -5,27 +5,15 @@
 // useful: https://opensource.apple.com/source/network_cmds/network_cmds-481.20.1/unbound/compat/memmove.c.auto.html
 int	main(void)
 {
-	char dest[20] = "hello";
-	char src[] = " world world world world world world world world world";
-	// strcpy(dest, src);
-	int r = strlcat(dest, src, 20);
-	// strncpy(dest, src, 2);
+	char s1[] = "hello";
+	char s2[] = "world";
 
-	for (int i = 0; i < 20; i++)
-	{
-		printf("%c", dest[i]);
-	}
-	puts("");
-	printf("strlcat: %d\n", r);
+	int r = ft_memcmp(s1, s2, 0);
 
-	char dest2[20] = "hello";
-	char src2[] = " world world world world world world world world world";
-	int r2 = ft_strlcat(dest2, src2, 20);
+	printf("%d\n", r);
 
-	for (int i = 0; i < 20; i++)
-	{
-		printf("%c", dest2[i]);
-	}
-	puts("");
-	printf("ft_strlcat: %d\n", r2);
+	// if (p)
+	// 	printf("%p: %c\n", p, *p);
+	// else
+	// 	puts("nullptr");
 }
