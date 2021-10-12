07e190eb0f9506152da8db59c31f7967e18663f2
demicuz <pure.demicube@gmail.com>
2021-10-13 00:29:35 +0300

    .

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -3,31 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psharen <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: psharen <psharen@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/09 18:19:57 by psharen           #+#    #+#             */
-/*   Updated: 2021/10/09 18:20:10 by psharen          ###   ########.fr       */
+/*   Created: 2000/01/01 20:20:20 by psharen           #+#    #+#             */
+/*   Updated: 2000/01/01 20:20:20 by psharen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// TODO check for memory leaks
+#include <unistd.h>
 
-#include <stdio.h> // TODO
-#include <ctype.h> // TODO
-#include <string.h> // TODO
-#include "libft.h"
-
-// useful: https://opensource.apple.com/source/network_cmds/network_cmds-481.20.1/unbound/compat/memmove.c.auto.html
-
-void ft_wat(unsigned int i, char *c)
+void	ft_putchar_fd(char c, int fd)
 {
-	*c = *c + 1;
-}
-
-int	main(void)
-{
-	// for (int i = 0; i < 128; ++i)
-	// {
-	// 	printf("%d: %c\n", i, i);
-	// }
+	write(fd, &c, 1);
 }
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,31 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psharen <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: psharen <psharen@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/09 18:19:57 by psharen           #+#    #+#             */
-/*   Updated: 2021/10/09 18:20:10 by psharen          ###   ########.fr       */
+/*   Created: 2000/01/01 20:20:20 by psharen           #+#    #+#             */
+/*   Updated: 2000/01/01 20:20:20 by psharen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// TODO check for memory leaks
-
-#include <stdio.h> // TODO
-#include <ctype.h> // TODO
-#include <string.h> // TODO
+#include <unistd.h>
 #include "libft.h"
 
-// useful: https://opensource.apple.com/source/network_cmds/network_cmds-481.20.1/unbound/compat/memmove.c.auto.html
-
-void ft_wat(unsigned int i, char *c)
+void	ft_putnbr_fd(int n, int fd)
 {
-	*c = *c + 1;
-}
-
-int	main(void)
-{
-	// for (int i = 0; i < 128; ++i)
-	// {
-	// 	printf("%d: %c\n", i, i);
-	// }
+	if (n >= 0 && n <= 9)
+	{
+		ft_putchar_fd('0' + n, fd);
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		if (n <= -10)
+			ft_putnbr_fd(-(n / 10), fd);
+		ft_putnbr_fd(-(n % 10), fd);
+	}
+	else
+	{
+		if (n >= 10)
+			ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
 }
