4f7509ae8bd9fb12e8b740028434d476229ce740
demicuz <pure.demicube@gmail.com>
2021-10-13 00:13:19 +0300

    .

--- main.c
+++ main.c
@@ -19,17 +19,17 @@
 
 // useful: https://opensource.apple.com/source/network_cmds/network_cmds-481.20.1/unbound/compat/memmove.c.auto.html
 
-char ft_wat(unsigned int i, char c)
+void ft_wat(unsigned int i, char *c)
 {
-	return ((c + 1) % 128);
+	*c = *c + 1;
 }
 
 int	main(void)
 {
 	char s[] = "aaa123";
-	char *r = ft_strmapi(s, &ft_wat);
-	if (r)
-		printf("%s\n", r);
+	char *r = ft_striteri(s, &ft_wat);
+	if (s)
+		printf("%s\n", s);
 	else
 		puts("nullptr");
 	// for (int i = 0; i < 128; ++i)
