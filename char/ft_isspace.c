e2445b2350dbba3facbe2da98d82ee4648f13d16
Denis Mazurak <pure.demicube@gmail.com>
2022-08-19 05:11:05 +0300

    Add ft_isspace

--- char/ft_isspace.c
+++ char/ft_isspace.c
@@ -1,37 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_isspace.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: psharen <psharen@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2000/01/01 20:20:20 by psharen           #+#    #+#             */
-/*   Updated: 2000/01/01 20:20:20 by psharen          ###   ########.fr       */
+/*   Created: 2022/08/19 04:58:00 by psharen           #+#    #+#             */
+/*   Updated: 2022/08/19 05:04:03 by psharen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
-
-int	ft_atoi(const char *str)
+int	ft_isspace(char c)
 {
-	int	result;
-	int	sign;
-
-	result = 0;
-	sign = 1;
-	while (ft_isspace(*str))
-		str++;
-	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			sign *= -1;
-		str++;
-	}
-	while (*str >= '0' && *str <= '9')
-	{
-		result *= 10;
-		result += (*str - '0') * sign;
-		str++;
-	}
-	return (result);
+	if (c == ' ' || c == '\n' || c == '\v' || c == '\f' || c == '\r'
+		|| c == '\t')
+		return (1);
+	return (0);
 }
