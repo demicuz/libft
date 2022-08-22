638354af0bf49362c7976db76573154d09d59b2b
Denis Mazurak <pure.demicube@gmail.com>
2022-08-22 09:29:21 +0300

    Add comment to ft_substr_between

--- str/ft_substr_between.c
+++ str/ft_substr_between.c
@@ -6,12 +6,13 @@
 /*   By: psharen <psharen@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 08:53:52 by psharen           #+#    #+#             */
-/*   Updated: 2022/08/22 08:55:46 by psharen          ###   ########.fr       */
+/*   Updated: 2022/08/22 09:28:43 by psharen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
+// Chair pointed by *end is NOT included.
 char	*ft_substr_between(const char *start, const char *end)
 {
 	return (ft_substr(start, 0, end - start));
